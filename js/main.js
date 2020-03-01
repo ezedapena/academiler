4186b6c46dfbe6cfe2c9c665b9603ea222084824
ezedapena <ezequieldapena@gmail.com>
2020-03-01 17:20:38 -0300

    s

--- js/main.js
+++ js/main.js
@@ -29,6 +29,7 @@ firebase.initializeApp(firebaseConfig);
 const app = new Vue({
   el:"#app",
   data:{
+    search:'',
     pages:[],
     database: [],
     current: "principal",
@@ -39,46 +40,47 @@ const app = new Vue({
       app.current = "secondary"
     },
     async html(){
-        await firebase.database().ref('HTMLInfo/').once('value')
-          .then(function(snapshot){
-              app.database = snapshot.val()
-        })
+      await firebase.database().ref('HTMLInfo/').once('value')
+      .then(function(snapshot){
+        app.database = snapshot.val()
+      })
       app.current = "page"
       document.getElementById("cssArchivo").href="styles/html.css"
     },
     async css(){
       await firebase.database().ref('CSSInfo/').once('value')
-          .then(function(snapshot){
-              app.database = snapshot.val()
+      .then(function(snapshot){
+        app.database = snapshot.val()
       })
       app.current = "page"
       document.getElementById("cssArchivo").href="styles/css.css"
     },
     async js(){
       await firebase.database().ref('JSInfo/').once('value')
-          .then(function(snapshot){
-              app.database = snapshot.val()
+      .then(function(snapshot){
+        app.database = snapshot.val()
       })
       app.current = "page"
       document.getElementById("cssArchivo").href="styles/js.css"
     },
+    
     help(){
       
       switch(app.dialog){
         case "":
-          app.dialog= "one"
-          break;
+        app.dialog= "one"
+        break;
         case "one":
-          app.dialog = "two"
-          break;
+        app.dialog = "two"
+        break;
         case "two":
-          app.dialog = "three"
-          break;
+        app.dialog = "three"
+        break;
         case "three":
         app.dialog = "question"
         break;
       }
-
+      
     },
     cancel(){
       app.dialog = ""
@@ -99,22 +101,22 @@ const app = new Vue({
     })
     
   },
-
+  
   components:{
     principal:{
       template:`
       <div class="d-flex justify-content-center flex-wrap">
-        <img src="img/start.png" alt="start button" onclick="app.start()" class="linkimgs">
-        <img src="img/play.png" alt="play button"  class="linkimgs">
-        <img src="img/start.png" alt="start button" onclick="app.start()" class="linkimgs">
-
+      <img src="img/start.png" alt="start button" onclick="app.start()" class="linkimgs">
+      <img src="img/play.png" alt="play button"  class="linkimgs">
+      <img src="img/developers.png" alt="start button" onclick="app.start()" class="linkimgs">
+      
       </div>      
       `
     },
     secondary:{
       props:['array'],
       template:`
-    
+      
       <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
       <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -122,27 +124,27 @@ const app = new Vue({
       <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
       </ol>
       <template v-if="array.length != 0">
-        <div class="carousel-inner">
-        <div class="carousel-item active slide" id="linkHTML" :style="array[0].bg">
-          <div class="d-flex justify-content-center flex-column align-items-center slide-container" onclick="app.html()">
-            <img src="img/htmlogo.png" class="imgResponsive">
-            <img src="img/htmlimage.png" height="50%">
-          </div>
-        </div>
-        <div class="carousel-item slide" id="linkCSS" :style="array[1].bg">
-          <div class="d-flex justify-content-center flex-column align-items-center slide-container" onclick="app.css()">
-          <img src="img/csslogo.png" class="imgResponsive">
-          <img src="img/cssimage.png" height="50%">
-
-          </div>
-        </div>
-        <div class="carousel-item slide" id="linkJS" :style="array[2].bg">
-          <div class="d-flex justify-content-center flex-column align-items-center slide-container" onclick="app.js()">
-          <img src="img/jslogo.png" class="imgResponsive">
-          <img src="img/jsimage.png" height="50%">
-
-          </div>
-        </div>
+      <div class="carousel-inner">
+      <div class="carousel-item active slide" id="linkHTML" :style="array[0].bg">
+      <div class="d-flex justify-content-center flex-column align-items-center slide-container" onclick="app.html()">
+      <img src="img/htmlogo.png" class="imgResponsive">
+      <img src="img/htmlimage.png" height="50%">
+      </div>
+      </div>
+      <div class="carousel-item slide" id="linkCSS" :style="array[1].bg">
+      <div class="d-flex justify-content-center flex-column align-items-center slide-container" onclick="app.css()">
+      <img src="img/csslogo.png" class="imgResponsive">
+      <img src="img/cssimage.png" height="50%">
+      
+      </div>
+      </div>
+      <div class="carousel-item slide" id="linkJS" :style="array[2].bg">
+      <div class="d-flex justify-content-center flex-column align-items-center slide-container" onclick="app.js()">
+      <img src="img/jslogo.png" class="imgResponsive">
+      <img src="img/jsimage.png" height="50%">
+      
+      </div>
+      </div>
       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
@@ -159,37 +161,44 @@ const app = new Vue({
     page:{
       props:['var','labels'],
       components:{
-
+        
         one:{
           template: `<div id="dialog-div"><p>Hey you! <br> I´m Captain Miler</p></div>`
-      },
+        },
         two:{
           template: `<div id="dialog-div"><p>I need your help!<br> Did you study enough?</p></div>`
-      },
-      three:{
-        template:`<div id="dialog-div"><p> Mr. Code is back! <br>Are you ready to face him?</p></div>`
-      },
-      question:{
-        template: `<div id="fight"><button type="button" class="btn btn-success" href="">Let's go get him!</button>
-        <button type="button" class="btn btn-danger" onclick="app.cancel()">No, Let me study more</button></div>`
-      }
+        },
+        three:{
+          template:`<div id="dialog-div"><p> Mr. Code is back! <br>Are you ready to face him?</p></div>`
+        },
+        question:{
+          template: `<div id="fight"><button type="button" class="btn btn-success" href="">Let's go get him!</button>
+          <button type="button" class="btn btn-danger" onclick="app.cancel()">No, Let me study more</button></div>`
+        }
       },
       
       template:`
-      <div class="col-10">
-        <div id="vfor">
-          <div v-for="label in labels" class="divHTML border">
-            <div class="transparent-shadow"><h3 class="px-5 py-2">{{label.name}}</h3></div>    
-            <h4 class="px-5 py-2">{{label.title}}</h4>
-            <p class="px-5 py-2">{{label.detail}}</p>
-          </div>
-        </div>
+      <div class="col-12">
+      <div>
+      <div v-for="label in labels" class="divHTML border">
+      <div class="transparent-shadow"><h3 class="px-5 py-2">{{label.title}}</h3></div>    
+      <h4 class="px-5 py-2">{{label.name}}</h4>
+      <p class="px-5 py-2">{{label.detail}}</p>
+      </div>
+      </div>
       
-        <div id="SOS"><component :is="this.var"></component><img src="img/head.png"  onclick="app.help()" id="head"></div>
-        </diV>
+      <div id="SOS"><component :is="this.var"></component><img src="img/head.png"  onclick="app.help()" id="head"></div>
+      </diV>
       `
-    },
-        
+    }
+    
+  },
+  computed:{
+    filterTitle(){ 
+      this.database = this.database.filter(data => {
+        return data.title.toLowerCase().match(this.search.toLowerCase())
+      })
+    }
   }
   
 })
